804661d9c79416d3c802890d6ff09b722735f354
Andreas <anwa@anwa-MPB13-1602.local>
2016-07-15 07:42:50 +0200

    updated example

--- exampleProject.framer/framer/framer.generated.js
+++ exampleProject.framer/framer/framer.generated.js
@@ -573,7 +573,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","se
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"exampleProject-update.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"exampleProject.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
